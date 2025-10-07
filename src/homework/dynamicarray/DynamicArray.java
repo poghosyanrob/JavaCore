a787618909e1106d0fc8818a55597765b1471185
Robert Poghosyan <robert.poghosyan.g@gmail.com>
2025-10-07 18:54:17 +0400

    homework DynamicArray

--- src/homework/dynamicarray/DynamicArray.java
+++ src/homework/dynamicarray/DynamicArray.java
@@ -12,24 +12,76 @@ public class DynamicArray {
         array[size++] = value;
     }
 
-    private void extend(){
-        int [] copyArray = new int[array.length + 10];
+    private void extend() {
+        int[] copyArray = new int[array.length + 10];
         for (int i = 0; i < array.length; i++) {
             copyArray[i] = array[i];
         }
         array = copyArray;
     }
 
+    public void deleteByIndex(int index) {
+        if (-1 < index && index <= size) {
+            for (int i = index; i < size; i++) {
+                array[i] = array[i + 1];
+            }
+            --size;
+        } else {
+            System.out.println("index:" + index + "  element doesn't exist");
+        }
+    }
+
+
+    public void set(int index, int value) {
+        if (index > -1 && index < size) {
+            array[index] = value;
+        } else {
+            System.out.println("index:" + index + "  element doesn't exist");
+        }
+    }
+
+    public void add(int index, int value) {
+        if (index > -1 && index < size) {
+            ++size;
+            int num = array[index];
+            array[index] = value;
+            for (int i = size; i > index ; i--) {
+                array[i] =  array[i - 1];
+            }
+            array[index + 1] = num;
+        } else {
+            System.out.println("index:" + index + "  element doesn't exist");
+        }
+    }
+
+    public boolean exists(int value) {
+        for (int num : array){
+            if(num == value){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getIndexByValue(int value) {
+        for (int i = 0; i < size; i++) {
+            if(array[i] == value){
+                return i;
+            }
+        }
+        return -1;
+    }
 
     public void print() {
         for (int i = 0; i < size; i++) {
             System.out.print(array[i] + " ");
         }
+        System.out.println();
     }
 
     public int getByIndex(int index) {
         for (int i = 0; i < size; i++) {
-            if(index == i){
+            if (index == i) {
                 return array[i];
             }
         }
--- src/homework/dynamicarray/DynamicArrayDemo.java
+++ src/homework/dynamicarray/DynamicArrayDemo.java
@@ -5,14 +5,29 @@ public class DynamicArrayDemo {
 
         DynamicArray num = new DynamicArray();
 
-        for (int i = 5; i < 19; i++) {
+        for (int i = 1; i < 15; i++) {
             num.add(i);
         }
 
-        System.out.println(num.getByIndex(54));
-        System.out.println(num.getByIndex(4));
         num.print();
-
-
+        System.out.println(num.getByIndex(4));
+        num.deleteByIndex(5);
+        num.print();
+        num.deleteByIndex(-35);
+        num.set(4,101);
+        num.print();
+        num.set(-5,12);
+        num.add(2,456);
+        num.print();
+        num.add(67,74);
+        System.out.println(num.exists(101));
+        System.out.println(num.exists(9854));
+        num.add(3,14);
+        num.print();
+        System.out.println(num.getIndexByValue(14));
+        num.add(0,14);
+        num.print();
+        System.out.println(num.getIndexByValue(14));
+        System.out.println(num.getIndexByValue(0));
     }
 }
